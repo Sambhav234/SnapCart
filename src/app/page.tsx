1c61c3911a682589d16670851ab0b0c687a4c00e
Sambhav Mishra <sambhavmishra234@gmail.com>
2026-02-06 22:07:35 +0530

    HomeSection Images and Search Bar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import User from "@/models/user_model";
 import connectDB from "@/lib/db";
 import { auth } from "@/auth";
 import EditRole from "@/components/EditRole";
+import Nav from "@/components/Nav";
+import UserDashboard from "../components/UserDashboard";
 export default async function Home() {
   await connectDB()
   const session=await auth();
@@ -14,9 +16,11 @@ export default async function Home() {
     return <EditRole/>
   }
 
+  const parsedUser=JSON.parse(JSON.stringify(user))
   return (
    <div>
-    
+    <Nav user={parsedUser}/>
+    <UserDashboard/>
    </div>
   );
 }
